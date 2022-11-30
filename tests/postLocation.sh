e8e6384c0c1fd28f2e4063c6e26205862d63964f
Yogesh Dhimate <dhimate@gmail.com>
2022-11-30 13:23:17 -0600

    Fixed the deployment issue for empty zip upload

--- tests/postLocation.sh
+++ tests/postLocation.sh
@@ -2,7 +2,7 @@ curl -XPOST \
   --url ${API_GW_URL} \
   --header 'content-type: application/json' \
   --data '{
-  "unicornName": "Vaidehi",
+  "unicornName": "John",
   "longitude": "13.404954",
   "latitude": "52.520008"
 }' 
